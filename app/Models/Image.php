9b0e2e2c2f986d3fd01706e4b86d70ef2a5d77c3
Emrullah Erbek <emrullah.erbek21@ogr.atauni.edu.tr>
2026-08-25 17:15:02 +0300

    feat: image modeli olusturuldu

---
+++ app/Models/Image.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    protected $fillable = [
+    'name',
+    'description',
+    'mime_type',
+    'folder',
+    'file_checksum',
+];
+}
