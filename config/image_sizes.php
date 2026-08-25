4c7baea987e9ca3783076fc3a9c407018e19df43
Emrullah Erbek <emrullah.erbek21@ogr.atauni.edu.tr>
2026-08-25 17:15:08 +0300

    feat: gorsel ebat ayarlari config dosyasina eklendi

---
+++ config/image_sizes.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'sizes' => [
+        '640x360'  => ['width' => 640, 'height' => 360],
+        '300x250'  => ['width' => 300, 'height' => 250],
+        '640xauto' => ['width' => 640, 'height' => null],
+    ],
+
+    'quality' => 85,
+];
